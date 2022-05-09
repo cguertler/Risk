a4ff5a3e667fd0c2205cf3e5d95f9b9f5d2aa195
Christian Guertler <cagbelt@gmail.com>
2022-05-09 10:39:50 -0600

    Code completed.

--- Risk/src/Risk.java
+++ Risk/src/Risk.java
@@ -7,8 +7,14 @@ public class Risk {
     static int redWin;
     static int whiteWin;
     static int tie;
+    static int totalNumberOfOutcomes;
 
     public static void main(String[] args){
+        redWin = 0;
+        whiteWin = 0;
+        tie = 0;
+        totalNumberOfOutcomes = 0;
+
 
         for (int r1=1; r1 <= 6; r1++){
             for (int r2=1; r2 <= 6; r2++){
@@ -20,20 +26,52 @@ public class Risk {
                             red[2] = r3;
                             white[0] = w1;
                             white[1] = w2;
-                            if(red[2]< 2){
-                                System.out.print("Before Sort: ");
-                                System.out.print(white[0]);
-                                System.out.println(white[1]);
-                                Arrays.sort(white, Collections.reverseOrder());
-                                System.out.print("After Sort:  ");
-                                System.out.print(white[0]);
-                                System.out.println(white[1]);
+                            Arrays.sort(red, Collections.reverseOrder());
+                            Arrays.sort(white, Collections.reverseOrder());
+                            if (white[0] < red [0]) {    //red wins first pair
+                                if (white[1] < red[1]) { //red wins second pair
+                                    redWin++;
+                                } else { //red and white both win 1
+                                    tie++;
+                                }
+                            } else if (white[0] >= red[0]) { //white wins first pair
+                                if (white[1] < red[1]) { //red wins second pair
+                                    tie++;
+                                } else { // white wins second pair
+                                    whiteWin++;
+                                }
                             }
+                            totalNumberOfOutcomes++;
                         }
                     }
                 }
             }
         }
-        System.out.print("Hello!");
+
+        double wins;
+        double outcomes;
+        double percent;
+
+        outcomes = totalNumberOfOutcomes;
+
+        System.out.print("Red Wins: " + redWin + " ... ");
+        wins = redWin;
+        percent = (wins/outcomes) * 100;
+        System.out.println(percent + "%");
+
+        System.out.print("White Wins: "+ whiteWin + " ... ");
+        wins = whiteWin;
+        percent = (wins/outcomes) * 100;
+        System.out.println(percent + "%");
+
+        System.out.print("Ties: " + tie + " ... ");
+        wins = tie;
+        percent = (wins/outcomes) * 100;
+        System.out.println(percent + "%");
+
+        int totalCalculatedOutcomes = redWin + whiteWin + tie;
+        System.out.println("Total Calculated Outcomes: " + totalCalculatedOutcomes);
+
+        System.out.println("Total Number of Outcomes: " + totalNumberOfOutcomes);
     }
 }
